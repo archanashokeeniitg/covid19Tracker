1f3f853f60a1ff3b07433d3c56db8c32ecc4ce40
Archana Shokeen <archanashokeen@Archanas-MacBook-Air.local>
2020-06-30 13:38:12 -0700

    Updates

---
+++ precache-manifest.29e69475e8c6806379381ba6c24663cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9cff65e03d8c765bc370a23906bacb",
+    "url": "/covid19Tracker/index.html"
+  },
+  {
+    "revision": "a79e4b2f7c503f90d1d0",
+    "url": "/covid19Tracker/static/css/main.43065288.chunk.css"
+  },
+  {
+    "revision": "4a7674f2742ed688fbe4",
+    "url": "/covid19Tracker/static/js/2.88e2715b.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid19Tracker/static/js/2.88e2715b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a79e4b2f7c503f90d1d0",
+    "url": "/covid19Tracker/static/js/main.d817bd6e.chunk.js"
+  },
+  {
+    "revision": "04b23a536b5c8021bbc8",
+    "url": "/covid19Tracker/static/js/runtime-main.9648791b.js"
+  },
+  {
+    "revision": "8cb8305f0cc991bb9da7720d13777b27",
+    "url": "/covid19Tracker/static/media/covid19.8cb8305f.jpg"
+  }
+]);
